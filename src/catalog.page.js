2d925087312a977a22dab28ff3fc13740274a604
Muhammad Azamuddin <azamuddin@live.com>
2016-10-02 20:29:01 +0700

    Add MobX and Stage-0 Babel Presets

--- src/catalog.page.js
+++ src/catalog.page.js
@@ -5,10 +5,18 @@ import React, { Component } from 'react'
 import {render} from 'react-dom'
 import { syncReduxAndRouter } from 'react-router-redux'
 import {hashHistory} from 'react-router'
-import routes from './routes/index'
+import { Route, IndexRoute} from 'react-router'
 
 let store = configureStore()
 let rootElement = document.getElementById('render')
 
+import CalculatorLayout from './layouts/Calculator'
+import CatalogIndex from './views/catalog'
+
+let routes = (
+  <Route path="/" component={CalculatorLayout}>
+    <IndexRoute component={CatalogIndex} />
+  </Route>
+)
 
 render(<Root routes={routes} history={hashHistory} />, rootElement)
